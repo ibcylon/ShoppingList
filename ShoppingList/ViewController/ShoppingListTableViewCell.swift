92a0ba55df606080d984ceda475f1689cd996c5f
kangho lee <ibcylon@naver.com>
2021-11-02 19:21:54 +0900

    UI

---
+++ ShoppingList/ViewController/ShoppingListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingListTableViewCell.swift
+//  ShoppingList
+//
+//  Created by Kanghos on 2021/11/02.
+//
+
+import UIKit
+
+class ShoppingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
